0c95ecb07a5934264ab31cc3e8f9112a367c8c0b
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-08-28 15:34:56 +0600

    Hero section added

--- src/Layout/LandingPage/Landing.tsx
+++ src/Layout/LandingPage/Landing.tsx
@@ -1,11 +1,15 @@
+import Contact from "../../page/Contact/Contact";
+import Hero from "../../page/Hero/Hero";
 import Login from "../../page/Login/Login";
 import Testimonial from "../../page/Testimonial/Testimonial";
 
 const Landing = () => {
   return (
     <div>
+      <Hero />
       <Login />
       <Testimonial />
+      <Contact />
     </div>
   );
 };
