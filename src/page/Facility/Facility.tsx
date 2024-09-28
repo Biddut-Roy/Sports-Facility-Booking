60bfcb1dbaf5666096bf97c4497b94c5a5cb26c8
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-09-28 11:15:59 +0600

    set a padding Faacility

--- src/page/Facility/Facility.tsx
+++ src/page/Facility/Facility.tsx
@@ -1,6 +1,6 @@
 const Facility = () => {
   return (
-    <div>
+    <div className="pt-10">
       <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
         <div className="border-b mb-5 flex justify-between text-sm">
           <div className="text-indigo-600 flex items-center pb-2 pr-2 border-b-2 border-indigo-600 uppercase">
