6d22a8f02bfd1f1ffc64aaa7528c80435957f61b
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-09-30 21:33:54 +0600

    facility component complete

--- src/page/Facility/Facility.tsx
+++ src/page/Facility/Facility.tsx
@@ -78,68 +78,36 @@ const Facility = () => {
               key={_id}
               className="rounded overflow-hidden shadow-lg flex flex-col"
             >
-              <a href="#"></a>
               <div className="relative">
-                <a href="#">
+                <div>
                   <img
-                    className="w-full"
+                    className="w-full h-44 md:h-44 lg:h-64"
                     src={item?.image_url}
                     alt="Sunset in the mountains"
                   />
                   <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
-                </a>
-                <NavLink to={"/"}>
-                  <div className="text-xs absolute top-0 right-0 bg-indigo-600 px-4 py-2 text-white mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
-                    View Card
-                  </div>
-                </NavLink>
+                </div>
               </div>
               <div className="px-6 py-4 mb-auto">
-                <a
-                  href="#"
-                  className="font-medium text-lg  hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
-                >
+                <div className="font-medium text-lg  hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2">
                   {item?.name}
-                </a>
+                </div>
               </div>
               <div className="px-6 py-3 flex flex-row items-center justify-between bg-gray-100">
                 <span className="py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center">
-                  <svg
-                    height="20px"
-                    width="20px"
-                    version="1.1"
-                    id="Layer_1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 512 512"
-                    style={{ background: "new 0 0 512 512", fill: "blue" }}
-                  >
-                    <g>
-                      <g>
-                        <path d="M256,0C114.837,0,0,114.837,0,256s114.837,256,256,256s256-114.837,256-256S397.163,0,256,0z M277.333,256 c0,11.797-9.536,21.333-21.333,21.333h-85.333c-11.797,0-21.333-9.536-21.333-21.333s9.536-21.333,21.333-21.333h64v-128 c0-11.797,9.536-21.333,21.333-21.333s21.333,9.536,21.333,21.333V256z"></path>
-                      </g>
-                    </g>
-                  </svg>
-                  <span className="ml-1">{item?.pricePerHour} $</span>
+                  <span className="ml-1">Hour: {item?.pricePerHour}$</span>
                 </span>
 
                 <span className="py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center">
-                  <svg
-                    className="h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                    ></path>
-                  </svg>
-                  <span className="ml-1">39 Comments</span>
+                  <span className="ml-1">
+                    {" "}
+                    <button
+                      type="button"
+                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      <NavLink to={"/"}>View Card </NavLink>{" "}
+                    </button>
+                  </span>
                 </span>
               </div>
             </div>
